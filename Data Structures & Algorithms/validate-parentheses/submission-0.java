908dc8fd8649c25820a71528df72c546e7b6f0d0
Omkar Amale <coderomkar5@gmail.com>
2026-07-19 20:15:04 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() == 0 || s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '{') {
+                stack.push('}');
+            } else if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '[') {
+                stack.push(']');
+            }else {
+                    if (stack.empty() || stack.pop() != ch) {
+                        return false;
+                    }
+                }
+            }
+         return stack.empty();
+        }
+    }
