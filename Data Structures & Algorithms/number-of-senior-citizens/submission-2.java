6f702e0a049469f50ea5959c7477929762d2e7de
Omkar Amale <coderomkar5@gmail.com>
2026-07-30 20:37:37 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+
+        int count=0;
+
+        for(String s:details){
+            if(Integer.parseInt(""+s.charAt(11)+s.charAt(12))>60){
+                count++;
+            }
+        }
+        return count;
+    }
+}
